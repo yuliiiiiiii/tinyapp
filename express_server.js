aa3886476e8fa22ba85c9ba397fd66100fd3f4ba
Yuli Wang <yuliwang1007@gmail.com>
2023-08-08 02:26:50 -0400

    lint

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const PORT = 8080;
 
@@ -15,16 +15,16 @@ app.get("/", (request, response) => {//get in order to see your response! get is
 
 app.get("/urls.json", (request, response) => {
   response.json(urlDatabase);
-})
+});
 
 app.get("/hello", (request, response) => {
   response.send("<html><body>Hello <b>Word</b></body></html>\n");
 });
 
 app.get("/urls", (request, response) => {
-  const templateVars = {urls: urlDatabase}; //the variable needs to be inside an object so we can access values through keys 
+  const templateVars = {urls: urlDatabase}; //the variable needs to be inside an object so we can access values through keys
   response.render('urls_index', templateVars); //('the template to show HTML on the /urls web page', the variable whose value is an object to be referenced in the template)
-})
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
